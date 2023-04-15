e9b744c5ebb6a726499e218d847dbb2d42d9e4f9
valkyrie_pilot <valk@randomairborne.dev>
2023-04-14 22:06:34 -0600

    update aws sdk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const S3 = require('aws-sdk/clients/s3');
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const core = require('@actions/core');
 const fs = require('fs');
+const path = require('path');
 
 try {
     const endpoint = core.getInput('endpoint');
@@ -9,36 +10,35 @@ try {
     const bucket = core.getInput('bucket');
     const file = core.getInput('file');
     const destination = core.getInput('destination');
-
-    const s3 = new S3({
-        endpoint: endpoint,
+    const credentials = {
         accessKeyId: accesskeyid,
         secretAccessKey: secretaccesskey,
-        signatureVersion: 'v4',
+    }
+    const client = new S3Client({
+        endpoint: endpoint,
+        credentials: credentials,
     });
 
-    var uploadParams = { Bucket: bucket, Key: '', Body: '' };
 
     var fileStream = fs.createReadStream(file);
     fileStream.on('error', function (err) {
         console.log('File Error', err);
         core.setFailed(err);
     });
-    uploadParams.Body = fileStream;
+    let key = "";
     if (destination == "") {
-        var path = require('path');
-        uploadParams.Key = path.basename(file);
-    } else { uploadParams.Key = destination; }
-
-    // call S3 to retrieve upload file to specified bucket
-    s3.upload(uploadParams, function (err, data) {
-        if (err) {
-            console.log("Error", err);
-            core.setFailed(err);
-        } if (data) {
-            console.log("Upload Success", data.Location);
-        }
+        key = path.basename(file);
+    } else { key = destination; }
+    const command = new PutObjectCommand({
+        Bucket: bucket,
+        Key: key,
+        Body: fileStream,
     });
+    try {
+        await client.send(command);
+      } catch (err) {;
+        core.setFailed(err);
+      }
 } catch (error) {
     core.setFailed(error.message);
 }
